423d3a7fbb5d739fe8fc7b7e31c8c2d980f2212c
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2023-03-29 14:04:28 +0200

    Use code from gabo's branch

--- src/pull_data.py
+++ src/pull_data.py
@@ -1,15 +1,38 @@
 import json
 import sys
+from urllib.parse import quote
+
 from utils import dbconnector
 
 # get config
-with open("config.json") as f:
+with open("config.json", encoding="utf-8") as f:
     config = json.load(f)
-table_name = "dev" if config["dev"] else "prod"
+TABLENAME = "dev" if config["dev"] else "prod"
 
+
+query = f"""
+    SELECT  
+        timestamp,
+        Accelerometer_x,
+        Accelerometer_y,
+        Accelerometer_z,
+        Gyroscope_x,
+        Gyroscope_y,
+        Gyroscope_z,
+        Magnetometer_x,
+        Magnetometer_y,
+        Magnetometer_z,
+        activity,
+        hash
+    FROM 
+        {TABLENAME};
+    """
+
+# convert query to url format
+query = quote(query)
 
 # get data
-data = dbconnector.Database().get_data(f"SELECT * FROM {table_name} LIMIT 10")
+data = dbconnector.Database().get_data(query)
 
 # save data as parquet
 out_filename = sys.argv[1]
--- src/utils/dbconnector.py
+++ src/utils/dbconnector.py
@@ -1,16 +1,24 @@
+""" QuestDB Connector """
+
+# Base Modules
 import io
 import json
 import requests
 
+# 3rd Party Modules
 import polars as pl
 
 # read config
-with open("config.json") as f:
+with open("config.json", encoding="utf-8") as f:
     config = json.load(f)
 questdb_settings = config["questdb"]
 
 
 class Database:
+    """
+    Class to connect to QuestDB and get data
+    """
+
     def __init__(self):
         pass
 
@@ -19,7 +27,7 @@ class Database:
         Gets data from a query
 
         Args:
-            query (str): Query to run
+            query (str): Query to run (must contain timestamp and hash columns)
 
         Returns:
             data (polars.DataFrame): Dataframe with the data
@@ -28,13 +36,13 @@ class Database:
         url = f"http://{questdb_settings['host']}:{questdb_settings['port']}/exp?query={query}"
 
         # get data
-        r = requests.get(url)
+        request = requests.get(url, timeout=600)
 
         # read data into polars dataframe
-        data = pl.read_csv(io.StringIO(r.text))
+        data = pl.read_csv(io.StringIO(request.text))
 
         # convert timestamp to datetime
-        data = data.with_column(
+        data = data.with_columns(
             pl.col("timestamp")
             .str.strptime(pl.Datetime, fmt="%Y-%m-%dT%H:%M:%S.%6fZ")
             .alias("timestamp")
@@ -43,7 +51,7 @@ class Database:
         # change columns with Float64 to Float32
         for col in data.columns:
             if data[col].dtype == pl.Float64:
-                data = data.with_column(pl.col(col).cast(pl.Float32).alias(col))
+                data = data.with_columns(pl.col(col).cast(pl.Float32).alias(col))
 
         # sort by insert hash and time
         data = data.sort(["hash", "timestamp"])
