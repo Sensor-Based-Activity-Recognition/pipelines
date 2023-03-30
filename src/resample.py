437398793a9aee0b036b020a6b8589ac0c00e351
Yvo Keller <keller.yvo@gmail.com>
2023-03-30 16:44:41 +0200

    save

---
+++ src/resample.py
@@ -0,0 +1,28 @@
+import sys
+import pandas as pd
+
+# get args
+input_filename = sys.argv[1]
+resample_frequency_hz = int(sys.argv[2])
+interpolation_method = sys.argv[3]
+
+
+print(f"Resampling {input_filename} to {resample_frequency_hz}Hz using {interpolation_method} interpolation")
+
+# read parquet file
+data = pd.read_parquet(input_filename)
+data = data.set_index("timestamp", drop=True)
+data = data.groupby("hash")
+
+data_resampled = []
+
+# resample by activity and combine in new dataframe
+for _, dataframe in data:
+    dataframe = dataframe.resample(f"{int(1E6/resample_frequency_hz)}us", origin="start").interpolate(method=interpolation_method)
+
+    # save to combined dataframe
+    data_resampled += [dataframe]
+
+data_resampled = pd.concat(data_resampled, ignore_index=True)
+
+data_resampled.to_parquet("data/resample.parquet", index=True)
