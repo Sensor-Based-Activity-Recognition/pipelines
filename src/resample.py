15b9fbef1c07876de9ee028b36b07969e9f16496
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-03-30 16:53:53 +0200

    reformatting :)

--- src/resample.py
+++ src/resample.py
@@ -6,7 +6,9 @@ input_filename = sys.argv[1]
 resample_frequency_hz = int(sys.argv[2])
 interpolation_method = sys.argv[3]
 
-print(f"Resampling {input_filename} to {resample_frequency_hz}Hz using {interpolation_method} interpolation")
+print(
+    f"Resampling {input_filename} to {resample_frequency_hz}Hz using {interpolation_method} interpolation"
+)
 
 # read parquet file
 data = pd.read_parquet(input_filename)
@@ -17,7 +19,9 @@ data_resampled = []
 
 # resample by activity and combine in new dataframe
 for _, dataframe in data:
-    dataframe = dataframe.resample(f"{int(1E6/resample_frequency_hz)}us", origin="start").interpolate(method=interpolation_method)
+    dataframe = dataframe.resample(
+        f"{int(1E6/resample_frequency_hz)}us", origin="start"
+    ).interpolate(method=interpolation_method)
 
     # save to combined dataframe
     data_resampled += [dataframe]
