22b61ac8f5d432d047d48e12babd6f0d9ab9e409
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-04-05 11:24:37 +0200

    formatting

--- src/train_test_split.py
+++ src/train_test_split.py
@@ -25,37 +25,59 @@ if __name__ == "__main__":
     # read dill file
     print("Reading dill file...")
     with open(input_filename, "rb") as fr:
-        data = [] # list with dicts { activity, person, hash, segment_id }
+        data = []  # list with dicts { activity, person, hash, segment_id }
         for key, segments in load(fr).items():
             for segment in segments:
-                data.append({
-                    "activity": key[0],
-                    "person": key[1],
-                    "hash": key[2],
-                    "segment_id": segment["segment_id"][0]
-                })
+                data.append(
+                    {
+                        "activity": key[0],
+                        "person": key[1],
+                        "hash": key[2],
+                        "segment_id": segment["segment_id"][0],
+                    }
+                )
 
     df = pd.DataFrame(data)
 
-    # split
+    # split nach segment
     if split_type == "segment":
-        train, test = train_test_split(df, test_size=split_ratio, random_state=random_seed, stratify=df[["activity"]] if stratified else None)
+        train, test = train_test_split(
+            df,
+            test_size=split_ratio,
+            random_state=random_seed,
+            stratify=df[["activity"]] if stratified else None,
+        )
+    # split nach person oder measurement
     elif split_type in ["measurement", "person"]:
-        group_by_columns = ["activity", ("hash" if split_type == "measurement" else "person")]
-        df_grouped = df.groupby(group_by_columns)\
-                            .agg({"segment_id": list})\
-                            .reset_index()   
-        train, test = train_test_split(df_grouped, test_size=split_ratio, random_state=random_seed, stratify=df_grouped[["activity"]] if stratified else None)
+        group_by_columns = [
+            "activity",
+            ("hash" if split_type == "measurement" else "person"),
+        ]
+        df_grouped = (
+            df.groupby(group_by_columns).agg({"segment_id": list}).reset_index()
+        )
+        train, test = train_test_split(
+            df_grouped,
+            test_size=split_ratio,
+            random_state=random_seed,
+            stratify=df_grouped[["activity"]] if stratified else None,
+        )
         train, test = train.explode("segment_id"), test.explode("segment_id")
+    # unbekannter split type
     else:
         raise ValueError(f"Unknown split type {split_type}")
-    
-    print(f"Train test split done: {len(train)} train, {len(test)} test, test-ratio {len(test) / (len(train) + len(test))}")
+
+    print(
+        f"Train test split done: {len(train)} train, {len(test)} test, test-ratio {len(test) / (len(train) + len(test))}"
+    )
 
     # dump, json file with list for train and test segment ids
     print("Dumping...")
     with open(output_filename, "w") as fw:
-        json.dump({
-            "train": train["segment_id"].values.tolist(),
-            "test": test["segment_id"].values.tolist()
-        }, fw)
+        json.dump(
+            {
+                "train": train["segment_id"].values.tolist(),
+                "test": test["segment_id"].values.tolist(),
+            },
+            fw,
+        )
